5a8df5406cb5c463d0d0a3d7652bc779004ab0cb
EdenGenetTasama <edentasama19@gmail.com>
2021-09-20 10:47:30 +0300

    .

--- JAVASCRIPT/cartPage.js
+++ JAVASCRIPT/cartPage.js
@@ -1,23 +1,32 @@
+let arrayOfCartObject = [
+      {id: allDateItem[0].id,img: allDateItem[0].img[0], name:allDateItem[0].name , price:allDateItem[0].price, },
+      {id: allDateItem[1].id, img: allDateItem[1].img[0], name:allDateItem[1].name , price:allDateItem[1].price, },
+      {id: allDateItem[2].id, img: allDateItem[2].img[2], name:allDateItem[2].name , price:allDateItem[2].price, }
+
+]
+console.log(arrayOfCartObject);
+
+
 firstTR.innerHTML = `
-<tr>
-<td><button type="button" id="buttonRemoveOne">REMOVE</button></td>
-<td id= imgFirstTd><img src="http://www.yo-yoo.co.il/coolpics/images/uploads/loveityoyo3.jpg" id="tableImg"></td>
-<td>Big red Heart</td>
-<td id="priceOne">100</td></tr>
+<tr id=${arrayOfCartObject[0].id}>
+<td><button type="button" id="buttonRemoveOne" onclick="spliceFromArray()" onclick="removeFromCart(${arrayOfCartObject[0].id})">REMOVE</button></td>
+<td id= imgFirstTd><img src=${arrayOfCartObject[0].img} id="tableImg"></td>
+<td>${arrayOfCartObject[0].name}</td>
+<td id="priceOne">${arrayOfCartObject[0].price}</td></tr>
 `;
 
-secondTR.innerHTML = `<tr>
-<td><button type="button" id="buttonRemoveTwo">REMOVE</button></td>
-<td id= imgSecondTd><img src="http://www.yo-yoo.co.il/coolpics/images/uploads/loveityoyo3.jpg"></td>
-<td>Big red Heart</td>
-<td id="priceTwo">200</td></tr>
+secondTR.innerHTML = `<tr id=${arrayOfCartObject[1].id}>
+<td><button type="button" id="buttonRemoveTwo" onclick="spliceFromArray()">REMOVE</button></td>
+<td id= imgSecondTd><img src=${arrayOfCartObject[1].img}></td>
+<td>${arrayOfCartObject[1].name}</td>
+<td id="priceTwo">${arrayOfCartObject[1].price}</td></tr>
 `;
 
-thirdTR.innerHTML = `<tr>
-<td><button type="button" id="buttonRemoveThree">REMOVE</button></td>
-<td id= imgThird><img src="http://www.yo-yoo.co.il/coolpics/images/uploads/loveityoyo3.jpg"></td>
-<td>Big red Heart</td>
-<td id="priceThree">500</td></tr>
+thirdTR.innerHTML = `<tr id=${arrayOfCartObject[2].id}>
+<td><button type="button" id="buttonRemoveThree" onclick="spliceFromArray()">REMOVE</button></td>
+<td id= imgThird><img src=${arrayOfCartObject[2].img}></td>
+<td>${arrayOfCartObject[2].name}</td>
+<td id="priceThree">${arrayOfCartObject[2].price}</td></tr>
 `;
 
 let priceOne = document.getElementById("priceOne").innerHTML;
@@ -42,13 +51,49 @@ totalPriceId.innerHTML = `<p>Total Price:<span>${variableOfAllPrice}</span></p>`
 
 
 /**********************************FOR TOMORROW STUCK HERE  ***********************/
-let buttonRemoveOne = document.getElementById("buttonRemoveOne");
-console.log(buttonRemoveOne);
 
-function removeItemOfCart(htmlElement) {
-      htmlElement.innerHTML = "";
-    }
+
+function spliceFromArray(){
+arrayOfCartObject.splice(arrayOfCartObject.id,1);
+ console.log(arrayOfCartObject);
+//  for (let i = 0; i < arrayOfCartObject.length; i++) {
+//        if(arrayOfCartObject[i].id == ""){
+//             arrayOfCartObject[i].id.style.display=none ;
+
+//        }
+
+//  }
+}
+
+
+ 
+// let mainTrID = document.getElementById("mainTrID").innerHTML;
+// let mainTrIDTwo = document.getElementById("mainTrIDTwo").innerHTML;
+// let mainTrIDThree = document.getElementById("mainTrIDThree").innerHTML;
+
+// let arrayOfTR = [{allDateItem[0].id , }];
+// console.log(allDateItem[0].id.innerHTML);
+
+
+
+function removeFromCart(NameOfId){
+      for (let i = 0; i < arrayOfCartObject.length; i++) { 
+            if( arrayOfCartObject[i] =="none"  ){
+                  aNameOfId.display="none" ;
+                  console.log(arrayOfCartObject[i]);
+}
+ }
+}
+
+
+
+// let buttonRemoveOne = document.getElementById("buttonRemoveOne");
+// console.log(buttonRemoveOne);
+
+// function removeItemOfCart(htmlElement) {
+//       htmlElement.innerHTML = "";
+//     }
     
-buttonRemoveOne.onclick(()=>{
-      removeItemOfCart(firstTR1)
-})
+// buttonRemoveOne.onclick(()=>{
+//       removeItemOfCart(firstTR1)
+// })
